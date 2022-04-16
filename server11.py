ccbea25fd19326d8b0774da39daae9896b36c3a3
Anais Lawson <anl2140@columbia.edu>
2022-04-16 17:26:55 -0400

    aligned info on learn pages
    I aligned the information but that also meant that I changed some of the data structures or added more to resemble the slides. I made the demands into a list and split text into text and summary

--- server11.py
+++ server11.py
@@ -12,8 +12,9 @@ lessons = {
         "title": "SETTING",
         "image": "https://i.ibb.co/tKTB75g/setter.png",
         "gif": "https://i.ibb.co/9cct0C4/setting.gif",
-        "text": " You are the quarterback of the team! Setters set the ball up for the attackers/hitters to score. A coach must be able to rely on the setter to make sound, consistent decisions.",
-        "demands": "1. Ability to set the ball to the left-front position. 2. Move all around the court 3. Set high enough and position properly for the hitter to attack.",
+        "text": "You are the quarterback of the team!",
+        "summary": "Setters set the ball up for the attackers/hitters to score. A coach must be able to rely on the setter to make sound, consistent decisions.",
+        "demands": ["1.Ability to set the ball to the left-front position", "2. Move all around the court", "3. Set high enough and position properly for the hitter to attack."],
         "next_lesson":"2"
     },
 
@@ -22,8 +23,9 @@ lessons = {
         "title": "LIBERO",
         "image": "https://i.ibb.co/4Vmy6mp/libero.png",
         "gif": "https://i.ibb.co/mBKLpGy/libero.gif",
-        "text": "You are the defender of the team. As the libero you know you’ll be receiving serves all day long. Adapt and make changes when your teammates are getting picked on.",
-        "demands": "1. Always play in the back row. 2. Receive opponent’s attacks and serves 3. Wear a different color jersey.",
+        "text": "You are the defender of the team.",
+        "summary": "As the libero you know you’ll be receiving serves all day long. Adapt and make changes when your teammates are getting picked on.",
+        "demands": ["1. Always play in the back row." ,"2. Receive opponent’s attacks and serves" ,"3. Wear a different color jersey."],
         "next_lesson":"3"  
     },
 
@@ -32,8 +34,9 @@ lessons = {
         "title": "OUTSIDE HITTER",
         "image": "https://i.ibb.co/K7c3Hfg/outsidehitter.png",
         "gif": "https://i.ibb.co/qx9J19b/outside-hitter.gif",
-        "text": "You are the “bag of tools”. You play both the front row and the back row and should develop a variety of ways to attack the ball.",
-        "demands": "1. After the serve place yourself to the left front position. 2. Read the opponent’s defense and call out hitters.",
+        "text": "You are the “bag of tools”. ",
+        "summary": "You play both the front row and the back row and should develop a variety of ways to attack the ball.",
+        "demands": ["1. After the serve place yourself to the left front position" ,"2. Read the opponent’s defense and call out hitters."],
         "next_lesson":"4"  
     },
 
@@ -42,8 +45,9 @@ lessons = {
         "title": "MIDDLE BLOCKING",
         "image": "https://i.ibb.co/HKZhsVf/middleblocker.png",
         "gif": "https://i.ibb.co/4SypYDP/blocker.gif",
-        "text": "You are the team’s tallest athlete! Your role is to block the opposing team’s attack so be ready for the opponent’s quick middle attacks. But also move to either side to help teammates close blocks with the opposite hitter and outside hitter.",
-        "demands": "1. Also work as a hitter to attack. 2. Quick evaluation skills to anticipate the opposing team’s attack.",
+        "text": "You are the team’s tallest athlete! ",
+        "summary": "Your role is to block the opposing team’s attack so be ready for the opponent’s quick middle attacks. But also move to either side to help teammates close blocks with the opposite hitter and outside hitter.",
+        "demands": ["1. Also work as a hitter to attack.","2. Quick evaluation skills to anticipate the opposing team’s attack."],
         "next_lesson":"5"
     },
 
@@ -52,8 +56,9 @@ lessons = {
         "title": "OPPOSITE HITTER",
         "image": "https://i.ibb.co/dGkJ8XH/oppositehitter.png",
         "gif": "https://i.ibb.co/MV62r1r/opposite.gif",
-        "text": "You most often scores the most points in the team - no pressure! Your role is to score and block the ball. Be the backup of the setter when the traditional setter is unable to get to the ball.",
-        "demands": "1. Attack the ball when receiving a set. 2. Work with the middle hitter on blocks. 3. Best for left-handed players.",
+        "text": "You most often scores the most points in the team - no pressure!",
+        "summary": "Your role is to score and block the ball. Be the backup of the setter when the traditional setter is unable to get to the ball.",
+        "demands": ["1. Attack the ball when receiving a set." ,"2. Work with the middle hitter on blocks." ,"3. Best for left-handed players."],
         "next_lesson":"end"
     },
 }
--- static/learn.js
+++ static/learn.js
@@ -1,19 +1,47 @@
 function displayInfo(lesson){
+    $("#header").empty()
     $("#info").empty()
-    let row = $("<div class='row'>")
-    $('#info').append(row)
-    let showinfo = $("<div style ='padding: 0px 20px 0px 0px;' class='col-md-4 col-sm-12'>")
-    $(row).append(showinfo)
+
+    let headerrow = $("<div class='row'>")
+    $('#header').append(headerrow)
+    let backbtninfo = $("<div class='col-md-2 col-sm-12'>")
+    $(headerrow).append(backbtninfo)
+    let backbtn =  "<button type='button' id='back'>←</button> "
+    $(backbtninfo).append(backbtn)
+    let headerinfo = $("<div class='col-md-10 col-sm-12'>")
+    $(headerrow).append(headerinfo)
     let title = "<div class='title'>"+lesson["title"]+"</div>"
-    $(showinfo).append(title)
+    $(headerinfo).append(title)
+// first row
+    let firstrow = $("<div class='row'>")
+    $('#info').append(firstrow)
+    let explanation = $("<div class='col-md-6 col-sm-12'>")
+    $(firstrow).append(explanation)
     let textinfo = "<div class='textinfo'>"+lesson["text"]+"</div>"
-    $(showinfo).append(textinfo)
-    let demands = "<div class='demands'>"+lesson["demands"]+"</div>"
-    $(showinfo).append(demands)
-    let image = "<div style='padding: 0px;'><img src='"+lesson["image"]+"' alt='picture of volleyball lesson' width=250px>"
-    $(showinfo).append(image)
-    let gif = "<div style='padding: 0px;'><img src='"+lesson["gif"]+"' alt='picture of volleyball gif' width=250px>"
-    $(showinfo).append(gif)
+    $(explanation).append(textinfo)
+    let summary = "<div class='summary'>"+lesson["summary"]+"</div>"
+    $(explanation).append(summary)
+    let image = "<div class='infograophicssize'><img src='"+lesson["image"]+"' alt='picture of volleyball lesson' class='pictureformat'>"
+    let imagespace = $("<div class='col-md-6 col-sm-12'>")
+    $(firstrow).append(imagespace)
+    $(imagespace).append(image)
+    // second row
+    let secondrow = $("<div class='row'>")
+    $('#info').append(secondrow)
+    let demandsinfo = $("<div class='col-md-6 col-sm-12'>")
+    $(secondrow).append(demandsinfo)
+    $(demandsinfo).append("<div class='subhead'>Strategic Demands</div>")
+    
+    $.each(lesson["demands"], function(key,value){
+        let demands = "<div class='demands'>"+value+"</div>"
+        $(demandsinfo).append(demands)    
+    }) 
+    let gifspace = $("<div class='col-md-6 col-sm-12'>")
+    $(secondrow).append(gifspace)
+    let gif = "<div class='infograophicssize'><img src='"+lesson["gif"]+"' alt='picture of volleyball gif' class='pictureformat'>"
+    $(gifspace).append(gif)
+
+    //button
     if(lesson["next_lesson"]!= "end"){
         let btn =  "<button type='button' id='learn'>Learn Strategy "+lesson["next_lesson"]+"/5 ➜</button> "
         $(".strategy_button_row").append(btn)
