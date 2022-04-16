8f98c22cc42c297a71feac55463106c0321399f7
Mari <88263804+marianamaher@users.noreply.github.com>
2022-04-16 02:50:32 -0400

    changed route to the one created by Anaïs

--- static/home_page.js
+++ static/home_page.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
 
     $("#homebuttons").click(function() {
 
-        window.location.href = "http://127.0.0.1:5000/strategy";
+        window.location.href = "http://127.0.0.1:5000/teamlayout";
 
     })
 
