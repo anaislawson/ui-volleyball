08928c99105b2dbb5b879094f8a184f6e5b18c23
delee19 <dl3410@columbia.edu>
2022-04-24 18:56:19 -0400

    drag/drop names working

--- static/quiz_level_3.js
+++ static/quiz_level_3.js
@@ -1,26 +1,22 @@
 // testing js dict 
 
-var roles =[       
-    {
-        '1': 'Setter',
-        '2': 'Libero',
-        '3': 'Outside hitter',
-        '4': 'Opposite hitter',
-        '5': 'Middle blocker'
-    }
+var roles =[
+     "Setter",
+     "Libero",
+     "Outside hitter",
+     "Opposite hitter",
+     "Middle blocker"
 ]       
 
-var ans = []
+var used = []
 function display_roles(roles){
     $("#roles").html("");
     console.log(roles)
     $.each(roles, function(index,value){
-        $.each(value, function(index,value){
-            let newRole = ('<div class="role row drag_box options_box">'+value+'</div>');
-            $("#roles").append(newRole);
-            $(".ui-widget-content").draggable({
-                revert: "invalid",
-            });
+        let newRole = ('<div class="role row drag_box options_box" data-role='+value+'>'+ value + '</div>');
+        $("#roles").append(newRole);
+        $(".ui-widget-content").draggable({
+            revert: "invalid",
         });
     });
 
@@ -28,10 +24,49 @@ function display_roles(roles){
     $(".role").addClass("draggable");
 }
 
+function display_answer(){
+    $.each(used, function(index, value){
+        //make the draggable name object
+        newName = $("<div class='role row options_box' data-role=" + value + ">").text(value);
+        $("#submission").append(newName);
+    });
+}
+
 $(document).ready(function() {
 
     display_roles(roles)
     $(".role").draggable({ cursor: "crosshair", revert: "invalid", helper: "clone", stack: ".name"});
+    $(".submit_box").addClass("droppable");
+
+    $(".submit_box").droppable({ accept: ".role",
+    drop: function(event, ui){
+        
+        //get dropped name
+        let role_name = $(ui.draggable).data("role");
+        let index = roles.indexOf(role_name);
+        //update names array
+
+        roles.splice($.inArray(role_name, roles), 1);
+
+        //update list1 array
+        used.push(role_name);
+        console.log(roles)
+        $("#submission").html("");
+        $("#submission").append("Your Answer:");
+        
+        //update the interface
+        display_roles(roles);
+        display_answer();
+
+
+    },
+    over: function(event,ui){
+        if($(this).has('.draggable').length) {
+            $(this).droppable('disable');
+        }
+    }
+
+}); 
 
      // droppable 
 
