7d2ad81f45e6e7836071b0fe072481ba2d47c97d
Anais Lawson <anl2140@columbia.edu>
2022-05-07 17:02:05 -0400

    small grammar edit

--- server11.py
+++ server11.py
@@ -18,7 +18,7 @@ lessons = {
         "gif": "https://i.ibb.co/9cct0C4/setting.gif", "text": "You are the quarterback of the team!",
         "summary": "Setters set the ball up for the attackers/hitters to score. A coach must be able to rely on the "
                    "setter to make sound, consistent decisions.",
-        "demands": ["1.Ability to set the ball to the left-front position", "2. Move all around the court",
+        "demands": ["1. Ability to set the ball to the left-front position", "2. Move all around the court",
                     "3. Set high enough and position properly for the hitter to attack."], "next_lesson": "2"},
 
     "2": {"lesson_id": "2", "title": "LIBERO", "image": "https://i.ibb.co/4Vmy6mp/libero.png",
