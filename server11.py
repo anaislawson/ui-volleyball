91258c11066bffa0e1aa2cc78713875d8e2a224d
Mariana Maher <mon2107@columbia.edu>
2022-05-09 14:16:26 -0400

    added missing gifs

--- server11.py
+++ server11.py
@@ -15,53 +15,53 @@ score_lvl_3 = 0
 lessons = {
 
     "1": {"lesson_id": "1", "title": "SETTING", "image": "/static/images/learning/setter.png",
-        "gif": "/static/images/learning/setting.gif", "text": "You are the quarterback of the team!",
-        "summary": "Setters set the ball up for the attackers/hitters to score. A coach must be able to rely on the "
-                   "setter to make sound, consistent decisions.",
-        "demands": ["1. Ability to set the ball to the left-front position", "2. Move all around the court",
-                    "3. Set high enough and position properly for the hitter to attack."], "next_lesson": "2"},
+          "gif": "/static/images/learning/setting.gif", "text": "You are the quarterback of the team!",
+          "summary": "Setters set the ball up for the attackers/hitters to score. A coach must be able to rely on the "
+          "setter to make sound, consistent decisions.",
+          "demands": ["1. Ability to set the ball to the left-front position", "2. Move all around the court",
+                      "3. Set high enough and position properly for the hitter to attack."], "next_lesson": "2"},
 
     "2": {"lesson_id": "2", "title": "LIBERO", "image": "/static/images/learning/libero.png",
-        "gif": "/static/images/learning/libero.gif", "text": "You are the defender of the team.",
-        "summary": "As the libero you know you’ll be receiving serves all day long. Adapt and make changes when your "
-                   "teammates are getting picked on.",
-        "demands": ["1. Always play in the back row.", "2. Receive opponent’s attacks and serves",
-                    "3. Wear a different color jersey."], "next_lesson": "3"},
+          "gif": "/static/images/learning/libero.gif", "text": "You are the defender of the team.",
+          "summary": "As the libero you know you’ll be receiving serves all day long. Adapt and make changes when your "
+          "teammates are getting picked on.",
+          "demands": ["1. Always play in the back row.", "2. Receive opponent’s attacks and serves",
+                      "3. Wear a different color jersey."], "next_lesson": "3"},
 
     "3": {"lesson_id": "3", "title": "OUTSIDE HITTER", "image": "/static/images/learning/layout-2.png",
-        "gif": "/static/images/learning/outside-hitter.gif", "text": "You are the “bag of tools”. ",
-        "summary": "You play both the front row and the back row and should develop a variety of ways to attack the "
-                   "ball.", "demands": ["1. After the serve, place yourself to the left front position",
-                                        "2. Read the opponent’s defense and call out hitters."], "next_lesson": "4"},
+          "gif": "/static/images/learning/outside-hitter.gif", "text": "You are the “bag of tools”. ",
+          "summary": "You play both the front row and the back row and should develop a variety of ways to attack the "
+          "ball.", "demands": ["1. After the serve, place yourself to the left front position",
+                               "2. Read the opponent’s defense and call out hitters."], "next_lesson": "4"},
 
     "4": {"lesson_id": "4", "title": "MIDDLE BLOCKING", "image": "/static/images/learning/middleblocker.png",
-        "gif": "/static/images/learning/blocker.gif", "text": "You are the team’s tallest athlete! ",
-        "summary": "Your role is to block the opposing team’s attack so be ready for the opponent’s quick middle "
-                   "attacks. But also move to either side to help teammates close blocks with the opposite hitter and "
-                   "outside hitter.", "demands": ["1. Also work as a hitter to attack.",
-                                                  "2. Quick evaluation skills to anticipate the opposing team’s attack."],
-        "next_lesson": "5"},
+          "gif": "/static/images/learning/blocker.gif", "text": "You are the team’s tallest athlete! ",
+          "summary": "Your role is to block the opposing team’s attack so be ready for the opponent’s quick middle "
+          "attacks. But also move to either side to help teammates close blocks with the opposite hitter and "
+          "outside hitter.", "demands": ["1. Also work as a hitter to attack.",
+                                         "2. Quick evaluation skills to anticipate the opposing team’s attack."],
+          "next_lesson": "5"},
 
     "5": {"lesson_id": "5", "title": "OPPOSITE HITTER", "image": "/static/images/learning/oppositehitter.png",
-        "gif": "/static/images/learning/opposite.gif",
-        "text": "You most often score the most points in the team - no pressure!",
-        "summary": "Your role is to score and block the ball. Be the backup of the setter when the traditional setter "
-                   "is unable to get to the ball.",
-        "demands": ["1. Attack the ball when receiving a set.", "2. Work with the middle hitter on blocks.",
-                    "3. Best for left-handed players."], "next_lesson": "end"}, }
+          "gif": "/static/images/learning/opposite.gif",
+          "text": "You most often score the most points in the team - no pressure!",
+          "summary": "Your role is to score and block the ball. Be the backup of the setter when the traditional setter "
+          "is unable to get to the ball.",
+          "demands": ["1. Attack the ball when receiving a set.", "2. Work with the middle hitter on blocks.",
+                      "3. Best for left-handed players."], "next_lesson": "end"}, }
 
 quiz_level_1 = {
     '1': {'question_id': '1', 'question': 'Which of the following is NOT required for a game of volleyball?',
-        'options': {'1': 'High Net', '2': 'Ball', '3': 'Whistle', '4': 'Court'}, 'answer_id': '3', 'next_id': '2'},
+          'options': {'1': 'High Net', '2': 'Ball', '3': 'Whistle', '4': 'Court'}, 'answer_id': '3', 'next_id': '2'},
     '2': {'question_id': '2', 'question': 'How many players are on a volleyball team?',
-        'options': {'1': 'five', '2': 'six', '3': 'seven', '4': 'eight'}, 'answer_id': '2', 'next_id': '3'},
+          'options': {'1': 'five', '2': 'six', '3': 'seven', '4': 'eight'}, 'answer_id': '2', 'next_id': '3'},
     '3': {'question_id': '3', 'question': 'What move is used to begin a round of volleyball?',
-        'options': {'1': 'spike', '2': 'block', '3': 'hit', '4': 'serve'}, 'answer_id': '4', 'next_id': '4'},
+          'options': {'1': 'spike', '2': 'block', '3': 'hit', '4': 'serve'}, 'answer_id': '4', 'next_id': '4'},
     '4': {'question_id': '4', 'question': 'How many touches does a team have in a round?',
-        'options': {'1': 'one', '2': 'two', '3': 'three', '4': 'four'}, 'answer_id': '3', 'next_id': '5'},
+          'options': {'1': 'one', '2': 'two', '3': 'three', '4': 'four'}, 'answer_id': '3', 'next_id': '5'},
     '5': {'question_id': '5',
-        'question': 'If a player grounds the ball on the opponent’s court, he/she scores a point by making a _____.',
-        'options': {'1': 'kill', '2': 'dig', '3': 'fault', '4': 'block'}, 'answer_id': '1', 'next_id': 'end'}, }
+          'question': 'If a player grounds the ball on the opponent’s court, he/she scores a point by making a _____.',
+          'options': {'1': 'kill', '2': 'dig', '3': 'fault', '4': 'block'}, 'answer_id': '1', 'next_id': 'end'}, }
 
 quiz_level_2 = {
     'roles': {'1': 'Setter', '2': 'Libero', '3': 'Outside hitter', '4': 'Outside hitter (left)', '5': 'Opposite hitter',
@@ -73,36 +73,37 @@ quiz_level_3 = {
                                                                'You receive serves all day long. \n '
                                                                'You always play in the back row \n '
                                                                'You should wear a different color jersey',
-        'answer_id': 3, 'next_id': '2'},
+                        'answer_id': 3, 'next_id': '2'},
 
-        '2': {'question_id': '2', 'question_prompt': 'You are the team\'s tallest athlete \n '
-                                                     'You should block the opposing team’s attack \n '
-                                                     'You should also hit to attack \n '
-                                                     'You should act as a decoy to help confuse opponents and spread out their blockers.',
-            'answer_id': 2, 'next_id': '3'},
+                  '2': {'question_id': '2', 'question_prompt': 'You are the team\'s tallest athlete \n '
+                        'You should block the opposing team’s attack \n '
+                        'You should also hit to attack \n '
+                        'You should act as a decoy to help confuse opponents and spread out their blockers.',
+                        'answer_id': 2, 'next_id': '3'},
 
-        '3': {'question_id': '3', 'question_prompt': 'You are the quarterback of the team. \n '
-                                                     'The coach relies on you to make sound consistent decisions. \n '
-                                                     'You should position properly for the hitter to '
-                                                     'attack. \n ', 'answer_id': 4, 'next_id': '4'},
+                  '3': {'question_id': '3', 'question_prompt': 'You are the quarterback of the team. \n '
+                        'The coach relies on you to make sound consistent decisions. \n '
+                        'You should position properly for the hitter to '
+                        'attack. \n ', 'answer_id': 4, 'next_id': '4'},
 
-        '4': {'question_id': '4', 'question_prompt': 'You most often scores the most points in the team. \n '
-                                                     'You should not only score but also block the ball.  \n '
-                                                     'Sometimes you are the backup of the setter. \n '
-                                                     'Work with the middle hitter on blocks.', 'answer_id': 5,
-            'next_id': '5'},
+                  '4': {'question_id': '4', 'question_prompt': 'You most often scores the most points in the team. \n '
+                        'You should not only score but also block the ball.  \n '
+                        'Sometimes you are the backup of the setter. \n '
+                        'Work with the middle hitter on blocks.', 'answer_id': 5,
+                        'next_id': '5'},
 
-        '5': {'question_id': '5', 'question_prompt': 'You are the “bag of tools". \n '
-                                                     'You play both the front row and the back row and should develop a variety of ways to '
-                                                     'attack the ball. \n '
-                                                     'You should read the opponent’s defense and call out hitters. \n',
-            'answer_id': 1, 'next_id': 'end'},
+                  '5': {'question_id': '5', 'question_prompt': 'You are the “bag of tools". \n '
+                        'You play both the front row and the back row and should develop a variety of ways to '
+                        'attack the ball. \n '
+                        'You should read the opponent’s defense and call out hitters. \n',
+                        'answer_id': 1, 'next_id': 'end'},
 
-    }}
+                  }}
 
 quiz_level_1_responses = {}
 
-quiz_level_2_empty_dic = {'empty_roles': {'1': '', '2': '', '3': '', '4': '', '5': '', '6': ''}, }
+quiz_level_2_empty_dic = {'empty_roles': {
+    '1': '', '2': '', '3': '', '4': '', '5': '', '6': ''}, }
 
 contents = {
     '1': {
@@ -212,6 +213,7 @@ contents = {
 }
 
 # ROUTES
+
 
 @app.route('/')
 def home_page():
@@ -334,10 +336,12 @@ def quiz_lv3(question_id):
         return render_template('quiz_final_score.html', score_lvl_1=score_lvl_1, score_lvl_2=score_lvl_2,
                                score_lvl_3=score_lvl_3, total_score=total_score)
 
-    active = {1: "Outside Hitter", 2: "Middle Blocker", 3: "Libero", 4: "Setter", 5: "Opposite Hitter"}
+    active = {1: "Outside Hitter", 2: "Middle Blocker",
+              3: "Libero", 4: "Setter", 5: "Opposite Hitter"}
 
     for x in range(int(question_id) - 1):
-        active.pop(int(quiz_level_3.get('questions').get(str(x + 1)).get('answer_id')))
+        active.pop(int(quiz_level_3.get('questions').get(
+            str(x + 1)).get('answer_id')))
 
     question = quiz_level_3['questions'][question_id]
     return render_template('quiz_level_3.html', roles=active, score=score_lvl_3, question=question,
